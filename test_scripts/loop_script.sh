8682d1cc7df534ae33f11f81d9328c952defe869
ramyakruw <31952139+ramyakruw@users.noreply.github.com>
2018-05-30 01:16:57 -0700

    Create loop_script.sh

---
+++ test_scripts/loop_script.sh
@@ -0,0 +1,19 @@
+echo "elapsed_time,sleep_time_ms"
+for ((i=1;i<=100;i++));
+do
+time1=( $(($(date +%s%N)/1000000)) )
+curl \
+--request POST \
+--data '{"server_ip":"xx.xxx.xx.xx","query":"select * from user limit 5", "output_to": ""}' \
+https://sfx01ruul4.execute-api.us-east-1.amazonaws.com/dev/myT2RdsCall
+time2=( $(($(date +%s%N)/1000000)) )
+elapsedtime=`expr $time2 - $time1`	
+sleeptime=`echo $onesecond - $elapsedtime | bc -l`
+sleeptimems=`echo $sleeptime/$onesecond | bc -l`
+echo "Run-$i,$json,$elapsedtime,$sleeptimems"
+if (( $sleeptime > 0 ))
+then
+sleep $sleeptimems
+fi
+echo "\n"
+done
